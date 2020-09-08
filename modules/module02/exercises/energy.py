2eea92110e0f1e93c2aaa9c33984a8e9f7733a44
Joshua Westgard <jwestgard@mac.com>
2020-09-07 22:03:24 -0400

    latest build

---
+++ modules/module02/exercises/energy.py
@@ -0,0 +1,210 @@
+# who were the biggest producers of solar and wind in 2009
+# hint: use a loop to iterate through the dictionary and 
+# keep track of the largest number for each with a conditional
+
+energy_2009 = {
+  "Mississippi": {
+    "solar": 0.0, 
+    "wind": 0.0
+  }, 
+  "Oklahoma": {
+    "solar": 0.0, 
+    "wind": 2271589.857
+  }, 
+  "Delaware": {
+    "solar": 0.0, 
+    "wind": 0.0
+  }, 
+  "Minnesota": {
+    "solar": 0.0, 
+    "wind": 4956986.761
+  }, 
+  "Illinois": {
+    "solar": 0.0, 
+    "wind": 2761152.218
+  }, 
+  "Arkansas": {
+    "solar": 0.0, 
+    "wind": 0.0
+  }, 
+  "New Mexico": {
+    "solar": 0.0, 
+    "wind": 1543715.0
+  }, 
+  "Indiana": {
+    "solar": 0.0, 
+    "wind": 1403191.55
+  }, 
+  "Maryland": {
+    "solar": 0.0, 
+    "wind": 0.0
+  }, 
+  "Louisiana": {
+    "solar": 0.0, 
+    "wind": 0.0
+  }, 
+  "Idaho": {
+    "solar": 0.0, 
+    "wind": 227027.926
+  }, 
+  "Wyoming": {
+    "solar": 0.0, 
+    "wind": 2213819.5519999997
+  }, 
+  "Tennessee": {
+    "solar": 0.0, 
+    "wind": 51747.0
+  }, 
+  "Arizona": {
+    "solar": 13759.445, 
+    "wind": 9555.0
+  }, 
+  "Iowa": {
+    "solar": 0.0, 
+    "wind": 7331390.872
+  }, 
+  "Michigan": {
+    "solar": 0.0, 
+    "wind": 289188.217
+  }, 
+  "Kansas": {
+    "solar": 0.0, 
+    "wind": 2385106.5
+  }, 
+  "Utah": {
+    "solar": 0.0, 
+    "wind": 64496.987
+  }, 
+  "Virginia": {
+    "solar": 0.0, 
+    "wind": 0.0
+  }, 
+  "Oregon": {
+    "solar": 0.0, 
+    "wind": 3372283.5930000003
+  }, 
+  "Connecticut": {
+    "solar": 0.0, 
+    "wind": 0.0
+  }, 
+  "Montana": {
+    "solar": 0.0, 
+    "wind": 810814.986
+  }, 
+  "California": {
+    "solar": 611763.387, 
+    "wind": 5764637.309
+  }, 
+  "Massachusetts": {
+    "solar": 67.335, 
+    "wind": 3797.825
+  }, 
+  "West Virginia": {
+    "solar": 0.0, 
+    "wind": 742439.0
+  }, 
+  "South Carolina": {
+    "solar": 0.0, 
+    "wind": 0.0
+  }, 
+  "New Hampshire": {
+    "solar": 0.0, 
+    "wind": 28465.641
+  }, 
+  "Wisconsin": {
+    "solar": 0.0, 
+    "wind": 1059126.19
+  }, 
+  "Vermont": {
+    "solar": 0.0, 
+    "wind": 11589.0
+  }, 
+  "Georgia": {
+    "solar": 0.0, 
+    "wind": 0.0
+  }, 
+  "North Dakota": {
+    "solar": 0.0, 
+    "wind": 2756288.9899999998
+  }, 
+  "Pennsylvania": {
+    "solar": 0.0, 
+    "wind": 921136.644
+  }, 
+  "Florida": {
+    "solar": 10225.0, 
+    "wind": 0.0
+  }, 
+  "Alaska": {
+    "solar": 0.0, 
+    "wind": 3062.442
+  }, 
+  "Kentucky": {
+    "solar": 0.0, 
+    "wind": 0.0
+  }, 
+  "Hawaii": {
+    "solar": 24.619, 
+    "wind": 213224.163
+  }, 
+  "Nebraska": {
+    "solar": 0.0, 
+    "wind": 288680.789
+  }, 
+  "Missouri": {
+    "solar": 0.0, 
+    "wind": 498515.151
+  }, 
+  "Ohio": {
+    "solar": 0.0, 
+    "wind": 15473.601
+  }, 
+  "Alabama": {
+    "solar": 0.0, 
+    "wind": 0.0
+  }, 
+  "New York": {
+    "solar": 0.0, 
+    "wind": 2258904.412
+  }, 
+  "South Dakota": {
+    "solar": 0.0, 
+    "wind": 392307.77499999997
+  }, 
+  "Colorado": {
+    "solar": 16530.477, 
+    "wind": 2942132.635
+  }, 
+  "New Jersey": {
+    "solar": 2437.768, 
+    "wind": 19149.957
+  }, 
+  "Washington": {
+    "solar": 0.0, 
+    "wind": 3538935.954
+  }, 
+  "North Carolina": {
+    "solar": 2321.627, 
+    "wind": 0.0
+  }, 
+  "District of Columbia": {
+    "solar": 0.0, 
+    "wind": 0.0
+  }, 
+  "Texas": {
+    "solar": 0.0, 
+    "wind": 19367238.862
+  }, 
+  "Nevada": {
+    "solar": 150858.353, 
+    "wind": 0.0
+  }, 
+  "Maine": {
+    "solar": 0.0, 
+    "wind": 260121.0
+  }, 
+  "Rhode Island": {
+    "solar": 0.0, 
+    "wind": 0.0
+  }
+}
